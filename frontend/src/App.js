d2397bcc2ac3d7c42bb45e94b3d9185f7dd99aa4
LenaLeona <cctnsw@gmail.com:q!:wq>
2020-12-05 22:58:27 -0800

    work on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from'./screens/HomeScreen'
+import ProductScreen from'./screens/ProductScreen'
 
 function App() {
   return (
-   <>
-     <h1>Welcome to Barnes & Noble</h1>
-   </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+      <Container>
+      <h1>Welcome to Barnes & Noble</h1>
+      </Container>
+      <Container>
+        <Route path='/' component={HomeScreen} exact />
+        {/* :id as the placeholder */}
+        <Route path='/product/:id' component={ProductScreen} />      
+      </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
-
 export default App;
